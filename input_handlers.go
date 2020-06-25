2783a749712628c94c14d91ce4581abe839d0ecd
bruce kellerman <bruce.kellerman@samsara.com>
2020-06-24 22:29:21 -0400

    Added key bindings for diagonal movement

--- input_handlers.go
+++ input_handlers.go
@@ -7,25 +7,45 @@ import (
 func handleKeys() *Action {
 	var action *Action
 
-	if rl.IsKeyPressed(rl.KeyUp) {
+	if rl.IsKeyPressed(rl.KeyUp) || rl.IsKeyPressed(rl.KeyK) {
 		action = &Action{actionType: Movement, actionMovement: &ActionMovement{
 			dx: 0,
 			dy: -1,
 		}}
-	} else if rl.IsKeyPressed(rl.KeyDown) {
+	} else if rl.IsKeyPressed(rl.KeyDown) || rl.IsKeyPressed(rl.KeyJ) {
 		action = &Action{actionType: Movement, actionMovement: &ActionMovement{
 			dx: 0,
 			dy: 1,
 		}}
-	} else if rl.IsKeyPressed(rl.KeyLeft) {
+	} else if rl.IsKeyPressed(rl.KeyLeft) || rl.IsKeyPressed(rl.KeyH) {
 		action = &Action{actionType: Movement, actionMovement: &ActionMovement{
 			dx: -1,
 			dy: 0,
 		}}
-	} else if rl.IsKeyPressed(rl.KeyRight) {
+	} else if rl.IsKeyPressed(rl.KeyRight) || rl.IsKeyPressed(rl.KeyL) {
 		action = &Action{actionType: Movement, actionMovement: &ActionMovement{
 			dx: 1,
 			dy: 0,
+		}}
+	} else if rl.IsKeyPressed(rl.KeyY) {
+		action = &Action{actionType: Movement, actionMovement: &ActionMovement{
+			dx: -1,
+			dy: -1,
+		}}
+	} else if rl.IsKeyPressed(rl.KeyU) {
+		action = &Action{actionType: Movement, actionMovement: &ActionMovement{
+			dx: 1,
+			dy: -1,
+		}}
+	} else if rl.IsKeyPressed(rl.KeyB) {
+		action = &Action{actionType: Movement, actionMovement: &ActionMovement{
+			dx: -1,
+			dy: 1,
+		}}
+	} else if rl.IsKeyPressed(rl.KeyN) {
+		action = &Action{actionType: Movement, actionMovement: &ActionMovement{
+			dx: 1,
+			dy: 1,
 		}}
 	} else if rl.IsKeyDown(rl.KeyLeftAlt) && rl.IsKeyPressed(rl.KeyEnter) {
 		action = &Action{actionType: Fullscreen}
